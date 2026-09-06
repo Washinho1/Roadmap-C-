ad1248f42f4de0912dfde7c90f011ba24fa3ed4f
Washington <washjr98@gmail.com>
2026-09-06 18:54:47 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            string nome = null;
+
+            Console.WriteLine("Me diga o seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Me diga a sua idade");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("seu nome é " + nome + " e sua idade daqui a 10 anos vai ser " + (idade + 10) );
+        }
+    }
+}
