b49caf0eabb5facb6a064b11d4c536e1d150dabf
Washington <washjr98@gmail.com>
2026-09-07 22:53:22 -0300

    Segundo commit segundo dia

---
+++ Dia 3 Operadores aritmeticos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("escolha o primeiro numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escola o segundo numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A soma dos dois daria: " +  (num1 + num2));
+            Console.WriteLine("A subtraçao dos dois daria: " + (num1 - num2));
+            Console.WriteLine("A divisao entre os dois daria: " + (num1 / num2));
+            Console.WriteLine("A multiplicaçao entre os dois daria: " + (num1 * num2));
+
+        }
+    }
+}
