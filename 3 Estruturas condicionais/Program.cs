9c28beb9cbd61ae3295b54300bbb0b7eb7f257de
Washington <washjr98@gmail.com>
2026-09-10 00:55:37 -0300

    third commit

---
+++ 3 Estruturas condicionais/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_4_Estruturas_condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            Console.WriteLine("Digite a nota do aluno");
+            nota = int.Parse(Console.ReadLine());
+
+            if (nota >= 9)
+            {
+                Console.WriteLine("Aluno tirou nota A");
+            }
+            else if (nota >= 7)
+            {
+                Console.WriteLine("Aluno tirou nota B");
+            }
+            else if (nota >= 5)
+            {
+                Console.WriteLine("Aluno tirou nota C");
+            }
+            else
+            {
+                Console.WriteLine("Aluno tirou nota F");
+            }
+        }
+        }
+    }
---
+++ 4 Laços de repetiçao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laços_de_repetiçao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+
+            Console.WriteLine("Digite a tabuada que deseja receber \n");
+            num1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(num1 + " x " + i + " = " + i * num1);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
